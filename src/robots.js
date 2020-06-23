a84d5c1d90c0ce463880bd3b247a3849263ec412
robhami <50372518+robhami@users.noreply.github.com>
2020-06-23 19:46:23 +0400

    add new project

---
+++ src/robots.js
@@ -0,0 +1,70 @@
+export const robots = [
+	{ 
+		id: 1,
+		name: 'Rob Hamilton',
+		username: 'RoboMan',
+		email: 'roboman@gmail.com'
+	},
+
+	{ 
+		id: 2,
+		name: 'Luisa Hamilton',
+		username: 'RoboWoMan',
+		email: 'roboWoMan@gmail.com'
+	},
+	{ 
+		id: 3,
+		name: 'Elodie Hamilton',
+		username: 'Eddie-bot',
+		email: 'Edmund-bot@gmail.com'
+	},
+	{ 
+		id: 4,
+		name: 'Jack Hamilton',
+		username: 'RoboMecahnic',
+		email: 'mech-robo@gmail.com'
+	},
+
+
+	{ 
+		id: 5,
+		name: 'Fred Hamilton',
+		username: 'RoboMan',
+		email: 'roboman@gmail.com'
+	},
+
+	{ 
+		id: 6,
+		name: 'Bottom Hamilton',
+		username: 'RoboWoMan',
+		email: 'roboWoMan@gmail.com'
+	},	
+	{ 
+		id: 7,
+		name: 'Idiot Hamilton',
+		username: 'Eddie-bot',
+		email: 'Edmund-bot@gmail.com'
+	},
+	{ 
+		id: 8,
+		name: 'Nigel Hamilton',
+		username: 'RoboMecahnic',
+		email: 'mech-robo@gmail.com'
+	},
+
+
+	{ 
+		id: 9,
+		name: 'DogBoy Hamilton',
+		username: 'RoboMan',
+		email: 'roboman@gmail.com'
+	},
+
+	{ 
+		id: 10,
+		name: 'DogGirl Hamilton',
+		username: 'RoboWoMan',
+		email: 'roboWoMan@gmail.com'
+	},	
+
+]
